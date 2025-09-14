3eec15bf3f0afd8ddba63617fb38ffaf63852b66
Animus004 <63477730+Animus004@users.noreply.github.com>
2025-09-14 17:03:58 +0530

    feat: Integrate Vercel Speed Insights
    Add `@vercel/speed-insights` package and integrate the `SpeedInsights` component into the application to monitor performance metrics.

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App';
 
 const rootElement = document.getElementById('root');
@@ -14,6 +15,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <SpeedInsights />
     </BrowserRouter>
   </React.StrictMode>
 );
